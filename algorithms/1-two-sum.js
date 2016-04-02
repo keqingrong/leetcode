c67afcdf8f82dd3637474561103a63e599fbd372
Qingrong <keqingrong1992@gmail.com>
2016-04-02 18:10:02 +0800

    Update 1-two-sum.js

--- algorithms/1-two-sum.js
+++ algorithms/1-two-sum.js
@@ -1,20 +1,41 @@
 /**
+ * solution 1
+ *
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-	var len = nums.length;
-	var i;
-	var map = {};
-	var num1, num2;
-	for(i = 0; i < len; i++){
-		num1 = nums[i];
-		num2 = target - num1;
-		if(map.hasOwnProperty(num2)){
-			return [i, map[num2]];
-		} else {
-			map[num1] = i;
-		}
-	}
+var twoSum = function (nums, target) {
+    var len = nums.length;
+    var i, j;
+    for (i = 0; i < len; i++) {
+        for (j = i + 1; j < len; j++) {
+            if (nums[j] === target - nums[i]) {
+                return [i, j];
+            }
+        }
+    }
+};
+
+/**
+ * solution 2
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    var len = nums.length;
+    var i;
+    var map = {};
+    var num1, num2;
+    for (i = 0; i < len; i++) {
+        num1 = nums[i];
+        num2 = target - num1;
+        if (map.hasOwnProperty(num2)) {
+            return [i, map[num2]];
+        } else {
+            map[num1] = i;
+        }
+    }
 };
