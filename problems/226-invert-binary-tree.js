ed2bef0ceb0f968749764326881f1375ad776496
keqingrong <keqingrong1992@gmail.com>
2016-09-11 14:01:26 +0800

    add invert binary tree

---
+++ problems/226-invert-binary-tree.js
@@ -0,0 +1,54 @@
+/**
+ *
+ * Invert a binary tree.
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ *
+ * to
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ *
+ *
+ *
+ */
+
+// Definition for a binary tree node.
+function TreeNode(val) {
+  this.val = val;
+  this.left = null;
+  this.right = null;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  if (root === null) {
+    return root;
+  } else {
+    var t = root.left;
+    root.left = root.right;
+    root.right = t;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+  }
+};
+
+var tree = new TreeNode(1);
+tree.left = new TreeNode(2);
+tree.right = new TreeNode(3);
+
+console.log(tree);
+console.log(invertTree(tree));
